2f9ae015963b7c1268df035f86d59c74eb5cb574
chinmayakain <chinmaynaik.009@gmail.com>
2022-04-05 22:50:57 +0530

    feat(controller): :art: Added the getTaskList and getTask api
    Added the getTaskList and getTask api

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a task name"],
+    trim: true,
+    maxlength: [60, "task name cannot exceed 30 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Tasks", TaskSchema);
